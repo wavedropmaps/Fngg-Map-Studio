3492bbae3cdcbf28bddabeb7fc3c27f9586f9915
FNGG Map Downloader <user@example.com>
2026-04-18 13:27:04 +1000

    Release v1.1.0: Fix download bug, add new map versions, add refresh feature

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "de.wauhundeland"
-version = "1.0-SNAPSHOT"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "FNGG Map Dumper"
-            packageVersion = "1.0.0"
+            packageVersion = "1.1.0"
             vendor = "WauHundeland"
 
             windows {
